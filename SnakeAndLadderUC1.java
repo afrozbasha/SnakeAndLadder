7d219b828b82e506bf3e6c3ef3c4ad3febe47fb1
afrozbasha <iafrozbasha@gmail.com>
2021-07-10 08:46:29 +0530

    Snake and Ladder game played with single player Start with 0 ps

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,13 @@
+package com.SnakeAndLadder;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int pos = 0;
+        System.out.println("Welcome to Snake And Ladder program");
+        System.out.println("Single player at start position "+ pos);
+        
+    }
+    
+}
