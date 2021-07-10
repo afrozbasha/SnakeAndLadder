a621e32fcadd5320381b0416cb845b5de4266d96
afrozbasha <iafrozbasha@gmail.com>
2021-07-10 08:31:15 +0530

    Snake and Ladder game played with single player

---
+++ SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.SnakeAndLadder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake And Ladder program");
+        
+    }
+}
